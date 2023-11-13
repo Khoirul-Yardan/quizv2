10cd13760ed1d9ae1d32178343f11e1c1709adc4
Khoirul-Yardan <yardanzorif447@gmail.com>
2023-11-13 07:44:20 +0700

    quizv2

---
+++ quiz.js
@@ -0,0 +1,75 @@
+alert("put in your score in the comment box")
+ 
+   //Questions array
+           // creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    number: 1,
+    question: "Pemilu pertama kali di Indonesia dilakukan pada tahun 1955 untuk memilih .......",
+    answer: "a.Anggota-anggota parlemen (DPR) dan Konstituanten",
+    options: [
+      "a.Anggota-anggota parlemen (DPR) dan Konstituanten",
+      "b.Presiden dan Wakil Presiden",
+      "c.Presiden, Wakil Presiden, dan Anggota-anggota DPR",
+      "d.Presiden, Gubernur, dan Bupati/Walikota"
+    ]
+  },
+    {
+    number: 2,
+    question: "Urutan yang mendapatkan suara terbanyak pada pemilu 1955 adalah...",
+    answer: "b.PNI, Masyumi, Nahdlatul Ulama, dan PKI",
+    options: [
+      "a.PNI, PKI, Masyumi, Nahdlatul Ulama",
+      "b.PNI, Masyumi, Nahdlatul Ulama, dan PKI",
+      "c.Nahdlatul Ulama, Masyumi, PKI, dan PNI ",
+      "d.PKI, Nahdlatul Ulama, PNI, dan Masyumi"
+    ]
+  },
+    {
+    number: 3,
+    question: "Dibawah ini yang bukan merupakan empat partai besar secara berturut-turut memenangkan kursi pada pemilu 1955 adalah...",
+    answer: "d. Golkar",
+    options: [
+      "a. Nahdlatul Ulama",
+      "b. PKI",
+      "c. PNI",
+      "d. Golkar"
+    ]
+  },
+    {
+    number: 4,
+    question: "Pemilihan umum pertama seharusnya dilakukan pada masa kabinet ali. Tetapi kemudian kabinet itu jatuh. Lalu berhasil dilaksanakan oleh kabinet...",
+    answer: "b. Kabinet Burhanuddin Harahap",
+    options: [
+      "a. Kabinet hatta",
+      "b. Kabinet Burhanuddin Harahap",
+      "c. Kabinet Wilopo",
+      "d. Kabinet Indonesia maju"
+    ]
+  },
+    {
+    number: 5,
+    question: "Apa isi dekret presiden yang keluar pada tanggal 5 juli 1959...",
+    answer: "c. Dibubarkan nya dewan konstituante dan berlaku nya UUD 1945",
+    options: [
+      "a. Dimasukkannya nilai nilai islam dalam Pancasila ",
+      "b. Adanya 3 usulan ideologis yaitu Pancasila,Islam, dan sosial eko ",
+      "c. Dibubarkan nya dewan konstituante dan berlaku nya UUD 1945",
+      "d. Pernyataan ganyang terhadap Malaysia"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
